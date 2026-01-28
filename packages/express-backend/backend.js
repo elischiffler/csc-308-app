1df5625fdd15eb556c9dc02f9c8e36e97e23bba7
Eli Schiffler <eschiffler1122@gmail.com>
2026-01-27 20:33:36 -0800

    Switched to mongoose functions

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,58 +1,47 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import userModel from "./user";
 
-const app = express();
-const port = 8000;
+mongoose.set("debug", true);
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
+mongoose
+  .connect("mongodb://localhost:27017/users", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
 
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
+function getUsers(name, job) {
+  let promise;
+  if (name === undefined && job === undefined) {
+    promise = userModel.find();
+  } else if (name && !job) {
+    promise = findUserByName(name);
+  } else if (job && !name) {
+    promise = findUserByJob(job);
+  }
+  return promise;
+}
 
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
+function findUserById(id) {
+  return userModel.findById(id);
+}
 
-const findUsersByNameAndJob = (name, job) => {
-  return users.users_list.filter(
-    (user) => user.name === name && user.job === job
-  );
-};
+function addUser(user) {
+  const userToAdd = new userModel(user);
+  const promise = userToAdd.save();
+  return promise;
+}
 
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
+function findUserByName(name) {
+  return userModel.find({ name: name });
+}
 
+function findUserByJob(job) {
+  return userModel.find({ job: job });
+}
 
 app.use(cors());
 app.use(express.json());
@@ -76,6 +65,20 @@ app.get("/users", (req, res) => {
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"];
   let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+// get a new user given a name and job
+app.get("/users/:name/:job", (req, res) => {
+  const name = req.params["name"];
+  const job = req.params["job"];
+  const result = users["users_list"].find(
+    (user) => user["name"] === name && user["job"] === job,
+  );
   if (result === undefined) {
     res.status(404).send("Resource not found.");
   } else {
@@ -110,3 +113,12 @@ app.delete("/users/:id", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+export default {
+  addUser,
+  getUsers,
+  findUserById,
+  findUserByName,
+  findUserByJob,
+};
